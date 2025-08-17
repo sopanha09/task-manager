6a51f4afcd3e9c92696b2da132f7d8a3f66b4c0d
sopanha09 <sopanhasous@gmail.com>
2025-08-17 22:51:11 +0700

    setup project, database & create model

---
+++ app/Models/TaskList.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TaskList extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+    ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
